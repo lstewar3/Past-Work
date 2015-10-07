7d49a8d24db20411ca156e8da4b462a2cd2a40c2
lstewar3 <stewartlylea@gmail.com>
2015-10-06 23:03:10 -0400

    Update CRAN_Data_Set_Analysis_Shortcut.R

--- CRAN_Data_Set_Analysis_Shortcut.R
+++ CRAN_Data_Set_Analysis_Shortcut.R
@@ -3,7 +3,7 @@
 #(1) Pick R data set to analyze 
 data() #this opens the list of available R data sets
 
-#(2) Once you have selected your data set. Open the code below in an r script and hit ctrl+F. This will start the search prompt. Search for Insert_Data_Set_Name and replace it with the name of your chosen dataset.
+#(2) Once you have selected your data set. Open the script below in an r script and hit ctrl+F. This will start the search prompt. Search for Insert_Data_Set_Name and replace it with the name of your chosen dataset.
 
 library(datasets) #download dat from CRAN
 data(Insert_Data_Set_Name) #loads data into workspace
@@ -15,4 +15,4 @@ row.names(m) #figure out how many observations the data has. Note this is also s
 colnames(m) #good practice to execute this function, but summary call below will show columns as well
 summary(m)# this will print off the mean,median, min, max, first and second quartiles of each column variable
 
-#(3) You may now run the program with the selected data set. 
+#(3) You may now run the code with the selected data set. 

89f5c78a3316ab2096d10ffef4d43d878dd3d2ae
lstewar3 <stewartlylea@gmail.com>
2015-10-21 15:49:01 -0400

    Create Ravens_XML_Web_Scraper.R

---
+++ Ravens_XML_Web_Scraper.R
@@ -0,0 +1,14 @@
+fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileUrl,useInternalNodes=TRUE)
+# use htmltreeparse instead of xml treeparese because this is an html file now from the web
+# use internal is true so that I can get all of the different nodes in the file
+record <- xpathSApply(doc,"//li[@class='record']",xmlValue)
+# look for elements that are list items li and have a particular class. class equals record here. will go through file and any time it sees a list item it will check and see if it is a record. if it is it will return the xml value # look on the raw data and you will see the list items.    
+ranking <- xpathSApply(doc,"//li[@class='ranking']",xmlValue)
+team <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+nextgame <- xpathSApply(doc,"//li[@class='nextGame']",xmlValue)
+
+team
+record
+ranking 
+nextgame 
